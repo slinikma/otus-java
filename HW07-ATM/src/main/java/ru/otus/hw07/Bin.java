ec8aa5519149343089c7b1125b3d23c6115b48dd
Машняга Никита <NAMashnyaga.SBT@sberbank.ru>
2020-02-11 19:42:33 +0300

    Minor fix

--- HW07-ATM/src/main/java/ru/otus/hw07/Bin.java
+++ HW07-ATM/src/main/java/ru/otus/hw07/Bin.java
@@ -8,13 +8,12 @@ public class Bin {
   private @Getter BigDecimal amount;
   private @Getter Coin coin;
 
-  // TODO: why is it better to use BigDecimal for money amount?
   public Bin(Coin coin, BigDecimal amount) {
     this.coin = coin;
     this.amount = amount;
   }
 
-  public BigDecimal addMoney(Coin coin, BigDecimal amount) {
+  public BigDecimal putCoins(Coin coin, BigDecimal amount) {
     if (!this.coin.equals(coin)) {
       throw new IllegalArgumentException("This bin can hold only " + coin.getCoinInfo() + " coins");
     } else {
@@ -24,7 +23,15 @@ public class Bin {
     return this.amount;
   }
 
-  public BigDecimal withdrawMoney(BigDecimal amount) {
-    return this.amount;
+  public BigDecimal getCoins(BigDecimal amount) {
+    // this.amount < amount
+    if (this.amount.compareTo(amount) == -1) {
+      amount = this.amount;
+      this.amount = new BigDecimal(0);
+      return amount;
+    } else {
+      this.amount.subtract(amount);
+      return amount;
+    }
   }
 }
--- HW07-ATM/src/main/java/ru/otus/hw07/BinFactory.java
+++ HW07-ATM/src/main/java/ru/otus/hw07/BinFactory.java
@@ -2,7 +2,6 @@ package ru.otus.hw07;
 
 import java.math.BigDecimal;
 
-// Simple factory??
 public class BinFactory {
   public static Bin getBin(Coin coin, BigDecimal amount) {
     return new Bin(coin, amount);
