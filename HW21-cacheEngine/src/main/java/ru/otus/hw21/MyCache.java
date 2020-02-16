0a259de1ec4fc83bb5aa1a81f1f317de526203fc
Машняга Никита <NAMashnyaga.SBT@sberbank.ru>
2020-02-16 18:00:16 +0300

    Fixed cache listener removing and trying to use weak reference with lambda listeners ...

--- HW21-cacheEngine/src/main/java/ru/otus/hw21/MyCache.java
+++ HW21-cacheEngine/src/main/java/ru/otus/hw21/MyCache.java
@@ -1,5 +1,11 @@
 package ru.otus.hw21;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.ref.PhantomReference;
+import java.lang.ref.Reference;
+import java.lang.ref.ReferenceQueue;
 import java.lang.ref.WeakReference;
 import java.util.Collection;
 import java.util.HashSet;
@@ -8,25 +14,43 @@ import java.util.WeakHashMap;
 
 public class MyCache<K, V> implements Cache<K, V> {
 
+  private static final Logger logger = LoggerFactory.getLogger(MyCache.class);
+
   private final Map<K, V> cache = new WeakHashMap();
   private final Collection<WeakReference<CacheListener>> listeners = new HashSet<>();
+  //создаем очередь ReferenceQueue
+  private static ReferenceQueue<CacheListener> refQueue = new ReferenceQueue<>();
 
   @Override
   public void put(K key, V value) {
+    Reference<? extends CacheListener> removedListenerRef;
+    while ((removedListenerRef = refQueue.poll()) != null) {
+      logger.info("Ref queue poll: {}", removedListenerRef);
+      this.removeListener(removedListenerRef.get());
+    }
+
     cache.put(key, value);
     for (var listener : listeners) {
-      if (listener != null) {
-        listener.get().notify(key, value, "put");
+      CacheListener<K,V> lst = listener.get();
+      if (lst != null) {
+        lst.notify(key, value, "put");
       }
     }
   }
 
   @Override
   public void remove(K key) {
+    Reference<? extends CacheListener> removedListenerRef;
+    while ((removedListenerRef = refQueue.poll()) != null) {
+      logger.info("Ref queue poll: {}", removedListenerRef);
+      this.removeListener(removedListenerRef.get());
+    }
+
     if (cache.containsKey(key)) {
       for (var listener : listeners) {
-        if (listener != null) {
-          listener.get().notify(key, cache.get(key), "remove");
+        CacheListener<K,V> lst = listener.get();
+        if (lst != null) {
+          lst.notify(key, cache.get(key), "remove");
         }
       }
     }
@@ -35,10 +59,17 @@ public class MyCache<K, V> implements Cache<K, V> {
 
   @Override
   public V get(K key) {
+    Reference<? extends CacheListener> removedListenerRef;
+    while ((removedListenerRef = refQueue.poll()) != null) {
+      logger.info("Ref queue poll: {}", removedListenerRef);
+      this.removeListener(removedListenerRef.get());
+    }
+
     if (cache.containsKey(key)) {
         for (var listener : listeners) {
-          if (listener != null) {
-            listener.get().notify(key, cache.get(key), "get");
+          CacheListener<K,V> lst = listener.get();
+          if (lst != null) {
+            lst.notify(key, cache.get(key), "get");
           }
         }
         return cache.get(key);
@@ -48,15 +79,66 @@ public class MyCache<K, V> implements Cache<K, V> {
 
   @Override
   public void addListener(CacheListener listener) {
-    this.listeners.add(new WeakReference<>(listener));
+    Reference<? extends CacheListener> removedListenerRef;
+    while ((removedListenerRef = refQueue.poll()) != null) {
+      logger.info("Ref queue poll: {}", removedListenerRef);
+      this.removeListener(removedListenerRef.get());
+    }
+
+    this.listeners.add(WeakElement.create(listener));
   }
 
   @Override
   public void removeListener(CacheListener listener) {
-    this.listeners.remove(listener);
+    this.listeners.remove(WeakElement.create(listener));
   }
 
   public int size() {
     return cache.size();
+  }
+
+
+  // Понял, что удаление из моего HashSet не работает,
+  // т.к. сильная ссылка на объект != слабой ссылке
+  // WeakElement сохраняет hash исходной сильной ссылки (объекта?) и переопределяет equals
+  // Данный кусочек кода подсмотрел
+  static private class WeakElement extends WeakReference {
+    private int hash; /* Hashcode of key, stored here since the key
+                               may be tossed by the GC */
+
+
+    private WeakElement(CacheListener o) {
+      super(o);
+      hash = o.hashCode();
+    }
+
+    // TODO: заменить Object на дженерик
+    private static WeakElement create(CacheListener o) {
+
+      //создаем Phantom Reference на объект типа BigObject и "подвязываем" ее на переменную a.
+      PhantomReference<CacheListener> phantom = new PhantomReference<>(o, refQueue);
+
+      return (o == null) ? null : new WeakElement(o);
+    }
+
+    /* A WeakElement is equal to another WeakElement iff they both refer to objects
+           that are, in turn, equal according to their own equals methods */
+    public boolean equals(Object o) {
+      if (this == o)
+        return true;
+      if (!(o instanceof WeakElement))
+        return false;
+      Object t = this.get();
+      Object u = ((WeakElement) o).get();
+      if (t == u)
+        return true;
+      if ((t == null) || (u == null))
+        return false;
+      return t.equals(u);
+    }
+
+    public int hashCode() {
+      return hash;
+    }
   }
 }
--- HW21-cacheEngine/src/test/java/ru/otus/hw21/CacheTest.java
+++ HW21-cacheEngine/src/test/java/ru/otus/hw21/CacheTest.java
@@ -49,4 +49,57 @@ public class CacheTest {
       logger.info("key:{}, value:{}", idx, cache.get(String.valueOf(idx)));
     }
   }
+
+  @Test
+  void gcListeners() throws InterruptedException {
+    Cache<String, Long> cache = new MyCache<>();
+    CacheListener<String, Long> listener1 =
+        (key, value, action) -> logger.info("[Listener 1] key:{}, value:{}, action: {}",  key, value, action);
+    CacheListener<String, Long> listener2 =
+        (key, value, action) -> logger.info("[Listener 2] key:{}, value:{}, action: {}",  key, value, action);
+    CacheListener<String, Long> listener3 =
+        (key, value, action) -> logger.info("[Listener 3] key:{}, value:{}, action: {}",  key, value, action);
+
+    cache.addListener(listener1);
+    cache.addListener(listener2);
+    cache.addListener(listener3);
+
+    logger.info("Before GC");
+    cache.put("1", 123L);
+
+    listener1 = null;
+    listener2 = null;
+    listener3 = null;
+
+    System.gc();
+    Thread.sleep(1000);
+
+    logger.info("After GC");
+    cache.put("1", 123L);
+  }
+
+  @Test
+  void removeListeners() throws InterruptedException {
+    Cache<String, Long> cache = new MyCache<>();
+    CacheListener<String, Long> listener1 =
+        (key, value, action) -> logger.info("[Listener 1] key:{}, value:{}, action: {}",  key, value, action);
+    CacheListener<String, Long> listener2 =
+        (key, value, action) -> logger.info("[Listener 2] key:{}, value:{}, action: {}",  key, value, action);
+    CacheListener<String, Long> listener3 =
+        (key, value, action) -> logger.info("[Listener 3] key:{}, value:{}, action: {}",  key, value, action);
+
+    cache.addListener(listener1);
+    cache.addListener(listener2);
+    cache.addListener(listener3);
+
+    logger.info("Before GC");
+    cache.put("1", 123L);
+
+    cache.removeListener(listener1);
+    cache.removeListener(listener2);
+    cache.removeListener(listener3);
+
+    logger.info("After GC");
+    cache.put("1", 123L);
+  }
 }
